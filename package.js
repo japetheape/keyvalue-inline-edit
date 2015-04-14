96a26ee10c5939bbf3882a572cb05742ad92b290
Jaap van der Meer <jaapvandermeer@gmail.com>
2015-04-14 13:36:24 +0200

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'japetheape:keyvalue-inline-edit',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
     summary: 'An inline key value editor, usable as supersimple CMS.',
   // URL to the Git repository containing the source code for this package.
